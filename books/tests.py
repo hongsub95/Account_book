e0c8f62b259f3e16122937d1a7b4fbe4d2dc53fc
hongsub <hong9506@gmail.com>
2022-12-22 23:01:39 +0900

    마지막 수정

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase,APIRequestFactory
+from users import models as User_models
 
 # Create your tests here.
+class AccountBookTest(APITestCase):
+    
+    def setUp(self) -> None:
+        user = User_models.User.objects.create(username="user001",email="test001@test001.com",gender="M",name="user001")
+        user.set_password("user001")
+        user.save
+        self.user=user
+    
+    def test_create_book(self):
+        response = self.client.post(f"account_book/api/{self.user.pk}/")
+        self.assertEqual(response.status_code,200)
+        self.client.force_login(self.user,)
+        response = self.client.post(f"account_book/api/{self.user.pk}/")
+    
--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,19 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
+from rest_framework import status
 
+class RegisterUserTest(APITestCase):
+    def test_register(self):
+        url = reverse('user/api/register')
+        user_data = {
+            "username":"user01",
+            "password":"user01",
+            "email":"test01@test01.com",
+            "gender":"M",
+            "name":"user01"
+        }
+        response = self.client.post(url,user_data)
+        print(self.assertEqual(response.status,200))
 
 
 
